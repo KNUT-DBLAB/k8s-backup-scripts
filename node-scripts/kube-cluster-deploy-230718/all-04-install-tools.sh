2db51e1549ae196eeec37fe350e17626bc065f32
Oh Youn Seok <oys1751@gmail.com>
2023-07-18 20:46:51 +0900

    except problematic buildah package

--- node-scripts/kube-cluster-deploy-230718/all-04-install-tools.sh
+++ node-scripts/kube-cluster-deploy-230718/all-04-install-tools.sh
@@ -4,5 +4,5 @@
 export K8S_VERSION=1.27.3-00
 
 sudo apt update
-sudo apt install -y kubeadm=$K8S_VERSION kubelet=$K8S_VERSION kubectl=$K8S_VERSION cri-o cri-o-runc podman buildah
+sudo apt install -y kubeadm=$K8S_VERSION kubelet=$K8S_VERSION kubectl=$K8S_VERSION cri-o cri-o-runc podman
 sudo apt-mark hold kubelet kubeadm kubectl
