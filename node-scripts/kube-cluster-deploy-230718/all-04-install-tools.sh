a80a332c97da60d59cc2cf9fd41b54ac694fa08a
Excute <oys1751@gmail.com>
2023-12-11 21:08:23 +0900

    fix k8s deploy scripts 04-01

--- node-scripts/kube-cluster-deploy-230718/all-04-install-tools.sh
+++ node-scripts/kube-cluster-deploy-230718/all-04-install-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set k8s version
-export K8S_VERSION=1.28.2-00
+export K8S_VERSION=1.28.2-1.1
 
 sudo apt update
 sudo apt install -y kubeadm=$K8S_VERSION kubelet=$K8S_VERSION kubectl=$K8S_VERSION cri-o cri-o-runc podman
