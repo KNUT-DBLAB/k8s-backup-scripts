f0cb950f0fcd16833978a07a5de9727d59f2e16f
Excute <oys1751@gmail.com>
2023-12-11 20:38:21 +0900

    fix k8s deploy scripts

--- node-scripts/kube-cluster-deploy-230718/all-02-crio-repo.sh
+++ node-scripts/kube-cluster-deploy-230718/all-02-crio-repo.sh
@@ -5,12 +5,13 @@
 
 # Set OS, k8s version
 export OS=xUbuntu_22.04
-export VERSION=1.27
+export VERSION=1.28
 
 # Install libseccomp
-sudo echo 'deb http://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
-sudo apt update
-sudo apt install -y -t buster-backports libseccomp2 || sudo apt update -y -t buster-backports libseccomp2
+# sudo echo 'deb http://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
+# sudo apt update
+# sudo apt install -y -t buster-backports libseccomp2 || sudo apt update -y -t buster-backports libseccomp2
+sudo apt install -y -t libseccomp2 || sudo apt update -y -t libseccomp2
 
 # Install CRI-O
 sudo echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" >/etc/apt/sources.list.d/devel-kubic-libcontainers-stable.list
--- node-scripts/kube-cluster-deploy-230718/all-03-k8s-repo.sh
+++ node-scripts/kube-cluster-deploy-230718/all-03-k8s-repo.sh
@@ -3,14 +3,14 @@
 # Reference: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#k8s-install-0
 
 # Set k8s version
-export K8S_VERSION=1.27.3-00
+# export K8S_VERSION=1.27.3-00
 
 # Install required packages
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
 # Download gpg key
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # Add to repository
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
