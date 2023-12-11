0de176972075128762c2436cb86b21af0a7615be
Excute <oys1751@gmail.com>
2023-12-11 21:30:06 +0900

    fix crio service, kubeadm init with crio socket

--- node-scripts/kube-cluster-deploy-230718/all-04-install-tools.sh
+++ node-scripts/kube-cluster-deploy-230718/all-04-install-tools.sh
@@ -6,3 +6,5 @@ export K8S_VERSION=1.28.2-1.1
 sudo apt update
 sudo apt install -y kubeadm=$K8S_VERSION kubelet=$K8S_VERSION kubectl=$K8S_VERSION cri-o cri-o-runc podman
 sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl restart crio
--- node-scripts/kube-cluster-deploy-230718/cp-01-kubeadm-init.sh
+++ node-scripts/kube-cluster-deploy-230718/cp-01-kubeadm-init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubeadm init --pod-network-cidr=10.244.0.0/16
+kubeadm init --pod-network-cidr="10.244.0.0/16" --cri-socket="unix:///var/run/crio/crio.sock"
