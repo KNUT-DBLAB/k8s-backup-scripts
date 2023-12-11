bd0520152e48481d9ed4e91be56d3fcf01acbe7f
Excute <oys1751@gmail.com>
2023-12-11 20:49:02 +0900

    fix k8s deploy scripts 02-02

--- node-scripts/kube-cluster-deploy-230718/all-02-crio-repo.sh
+++ node-scripts/kube-cluster-deploy-230718/all-02-crio-repo.sh
@@ -8,10 +8,9 @@ export OS=xUbuntu_22.04
 export VERSION=1.28
 
 # Install libseccomp
-# sudo echo 'deb http://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
-# sudo apt update
-# sudo apt install -y -t buster-backports libseccomp2 || sudo apt update -y -t buster-backports libseccomp2
-sudo apt install -y -t libseccomp2 || sudo apt update -y -t libseccomp2
+echo 'deb http://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
+apt update
+apt install -y -t buster-backports libseccomp2 || apt update -y -t buster-backports libseccomp2
 
 # Install CRI-O
 echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" >/etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
