eb36386e0f78952eb945a1354fc4a6044c656199
Excute <oys1751@gmail.com>
2023-12-11 21:02:26 +0900

    fix k8s deploy scripts 02-03

---
+++ inoti/summary.md
@@ -0,0 +1,14 @@
+# summary
+
+1. [02-all-crio](#02-all-crio)
+
+---
+
+## 02-all-crio
+
+- `/etc/apt/sources.list.d/`
+  - `devel:kubic:libcontainers:stable.list`
+  - `devel:kubic:libcontainers:stable:cri-o:$VERSION.list`
+- `/usr/share/keyrings`
+  - `libcontainers-archive-keyring.gpg`
+  - `libcontainers-crio-archive-keyring.gpg`
--- node-scripts/kube-cluster-deploy-230718/all-02-crio-repo.sh
+++ node-scripts/kube-cluster-deploy-230718/all-02-crio-repo.sh
@@ -8,9 +8,10 @@ export OS=xUbuntu_22.04
 export VERSION=1.28
 
 # Install libseccomp
-echo 'deb http://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
+# echo 'deb http://deb.debian.org/debian buster-backports main' >/etc/apt/sources.list.d/backports.list
 apt update
-apt install -y -t buster-backports libseccomp2 || apt update -y -t buster-backports libseccomp2
+# apt install -y -t buster-backports libseccomp2 || apt update -y -t buster-backports libseccomp2
+apt install -y -t libseccomp2 || apt update -y -t libseccomp2
 
 # Install CRI-O
 echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" >/etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
