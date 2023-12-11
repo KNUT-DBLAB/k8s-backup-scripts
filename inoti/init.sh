b22c59f67191e306b17879b0c13062a90ea5740b
excute <oys1751@gmail.com>
2023-12-11 20:11:12 +0900

    edit inoti scripts

---
+++ inoti/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+mkdir -p /home/oys/grad-exp/inoti/
+touch "/home/oys/grad-exp/inoti/inoti.csv"
+rm "/home/oys/grad-exp/inoti/inoti.csv"
+touch "/home/oys/grad-exp/inoti/inoti.csv"
+chmod ugo+rw "/home/oys/grad-exp/inoti/inoti.csv"
+
+cp -r ./ /home/oys/grad-exp/inoti/
+
--- inoti/inoti.sh
+++ inoti/inoti.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
-touch "/home/oys/bash-scripts/inoti.csv"
-rm "/home/oys/bash-scripts/inoti.csv"
-touch "/home/oys/bash-scripts/inoti.csv"
-chmod ugo+rwx "/home/oys/bash-scripts/inoti.csv"
+touch "/home/oys/grad-exp/inoti/inoti.csv"
+rm "/home/oys/grad-exp/inoti/inoti.csv"
+touch "/home/oys/grad-exp/inoti.csv"
+chmod ugo+rwx "/home/oys/grad-exp/inoti.csv"
 
 EXCLUDES_READ=""
 EXCLUDES=""
 
-readarray -t EXCLUDES_READ </home/oys/bash-scripts/inoti-excludes.txt
+readarray -t EXCLUDES_READ </home/oys/grad-exp/inoti-excludes.txt
 
 for i in "${!EXCLUDES_READ[@]}"; do
     if [ -z "${EXCLUDES}" ]; then
@@ -23,5 +21,5 @@ done
 printf "\nExcludes = ${EXCLUDES}\n"
 
 sudo inotifywait / -rmq -e create,modify,move,delete,delete_self,unmount \
-    --format "%T,%:e,%w%f" --timefmt "%y%m%d-%H:%M:%S" -o "/home/oys/bash-scripts/inoti.csv" --excludei "${EXCLUDES}"
+    --format "%T,%:e,%w%f" --timefmt "%y%m%d-%H:%M:%S" -o "/home/oys/grad-exp/inoti.csv" --excludei "${EXCLUDES}"
 
